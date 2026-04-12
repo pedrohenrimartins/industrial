a55150bef053329c146fb03ccaa9de4005ba9712
pedrohenrimartins <129549967+pedrohenrimartins@users.noreply.github.com>
2026-04-12 14:39:27 -0300

    terminei a pagina de inventario e corrigi os estilos globais e de alguns componentes

--- src/app/pages/inventario/inventario.ts
+++ src/app/pages/inventario/inventario.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderAdmin } from '../../shared/header-admin/header-admin';
+import { MenuAdminInventario } from '../../shared/menu-admin-inventario/menu-admin-inventario';
 
 @Component({
   selector: 'app-inventario',
-  imports: [HeaderAdmin],
+  imports: [HeaderAdmin, MenuAdminInventario],
   templateUrl: './inventario.html',
   styleUrl: './inventario.css',
 })
---
+++ src/app/shared/menu-admin-inventario/menu-admin-inventario.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-admin-inventario',
+  imports: [],
+  templateUrl: './menu-admin-inventario.html',
+  styleUrl: './menu-admin-inventario.css',
+})
+export class MenuAdminInventario {}
