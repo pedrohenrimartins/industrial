d4831f0d0a1ce4a48d23e150e7873152999a5bf4
RodrigoMeloTheo <rodrigomelotheo@gmail.com>
2026-04-13 21:43:02 -0300

    verificação de valores de login (teste)

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -8,4 +8,19 @@ import { Footer } from '../../shared/footer/footer';
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  titulo = 'Faça seu Login!'
+  login = ''
+  senha = ''
+  botaoDesabilitado: boolean = true;
+
+onBotaoClicado(){
+  if(this.login.trim() !== '' && this.senha.trim() !== ''){
+  alert("bem vindo!")
+  }
+  else{
+    alert("Preencha todos os campos!")
+  }
+}
+
+}
